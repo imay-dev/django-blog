09c44d46528847b7db1748bb8eee65f1e37e64fc
Maysam Rabipour <maysam.rabipour@gmail.com>
2020-06-20 00:28:25 +0430

    Change PostListView to post_list in views

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,16 +1,31 @@
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import ListView
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.core.mail import send_mail
 
 from .models import Post
 from .forms import EmailPostForm, CommentForm
 
 
-class PostListView(ListView):
-    model = Post
-    context_object_name = 'posts'
-    paginate_by = 3
-    template_name = 'blog/post/list.html'
+def post_list(request):
+    object_list = Post.published.all()
+
+    paginator = Paginator(object_list, 3)
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+
+    return render(
+        request,
+        'blog/post/list.html',
+        {
+            'page': page,
+            'posts': posts
+        }
+    )
 
 
 def post_detail(request, year, month, day, post):
