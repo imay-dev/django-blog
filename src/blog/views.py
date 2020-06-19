0fc994c5e4d490b78f8ae5b0a73debeb2d501855
Maysam Rabipour <maysam.rabipour@gmail.com>
2020-06-20 00:46:53 +0430

    Add Similar Posts feature to post_detail view method

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.core.mail import send_mail
+from django.db.models import Count
 
 from taggit.models import Tag
 
@@ -58,6 +59,12 @@ def post_detail(request, year, month, day, post):
     else:
         comment_form = CommentForm()
 
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids)\
+        .exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')) \
+        .order_by('-same_tags', '-publish')[:4]
+
     return render(
         request,
         'blog/post/detail.html',
@@ -65,7 +72,8 @@ def post_detail(request, year, month, day, post):
             'post': post,
             'comments': comments,
             'new_comment': new_comment,
-            'comment_form': comment_form
+            'comment_form': comment_form,
+            'similar_posts': similar_posts
         }
     )
 
