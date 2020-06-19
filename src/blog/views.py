0b4086f4e37b940a203cb416f407665036711382
Maysam Rabipour <maysam.rabipour@gmail.com>
2020-06-20 00:34:26 +0430

    Add Tag functionality to post_list method

--- src/blog/views.py
+++ src/blog/views.py
@@ -2,12 +2,19 @@ from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.core.mail import send_mail
 
+from taggit.models import Tag
+
 from .models import Post
 from .forms import EmailPostForm, CommentForm
 
 
-def post_list(request):
+def post_list(request, tag_slug=None):
     object_list = Post.published.all()
+    tag = None
+
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        object_list = object_list.filter(tags__in=[tag])
 
     paginator = Paginator(object_list, 3)
     page = request.GET.get('page')
@@ -23,7 +30,8 @@ def post_list(request):
         'blog/post/list.html',
         {
             'page': page,
-            'posts': posts
+            'posts': posts,
+            'tag': tag
         }
     )
 
