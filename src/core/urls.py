1cfa4f4cb1ce7b5da94cd1fe14a46e8fabc3ecca
Maysam Rabipour <maysam.rabipour@gmail.com>
2020-06-21 01:35:49 +0430

    Fix blog.sitemaps import in core urls

--- src/core/urls.py
+++ src/core/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.contrib.sitemaps.views import sitemap
 from django.urls import path, include
 
-from ..blog.sitemaps import PostSitemap
+from blog.sitemaps import PostSitemap
 
 sitemaps = {
  'posts': PostSitemap,
